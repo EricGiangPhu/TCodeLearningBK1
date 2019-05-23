c280c541527f334d515c98da80ecf409b3d56634
Eric Giang Phu <phugiang.dev.game@gmail.com>
2019-05-23 19:11:33 +0700

    them aaaaa

--- Bai1/Bai1/Program.cs
+++ Bai1/Bai1/Program.cs
@@ -7,6 +7,7 @@ namespace Bai1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! aaaaaaaa");
         }
     }
 }
